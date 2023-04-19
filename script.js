8cbece823d4e4da59549ed0393166cd46624927b
BAGGS271 <samuelbaggs@yahoo.com>
2023-04-19 16:32:59 +0100

    Added Function
    I've added a Function which prompts the player to input Rock, Paper or Scissors.

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //A Rock Paper Scissors game played against the computer in the console.
 
+// Selects Rock, Paper or Scissors for the computer.
+
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3) + 1;
     if (x === 1) {
-        console.log("Rock");
+        return("Rock");
     }
     else if (x === 2) {
-        console.log("paper");
+        return("paper");
     }
     else {
-        console.log("Scissors");
+        return("Scissors");
     }
+}
+
+function getPlayerChoice () {
+    let input = prompt("Type Rock, Paper or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper or Scissors");
+    }
+    input = input.toLowerCase();
+    console.log(input);
 }
