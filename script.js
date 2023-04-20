899bede98e541b24a3cb49f59ab26103f60b17ba
BAGGS271 <samuelbaggs@yahoo.com>
2023-04-20 09:57:17 +0100

    Added a working playRound function.
    Added a playRound function that displays the winner of the round in the terminal.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//A Rock Paper Scissors game played against the computer in the console.
+// A Rock Paper Scissors game played against the computer in the console.
 
-const myArray = ["Rock", "Paper", "Scissors"]
+const myArray = ["rock", "paper", "scissors"]
 
 // Selects Rock, Paper or Scissors for the computer.
 
@@ -8,11 +8,31 @@ function getComputerChoice() {
     return myArray[Math.floor(Math.random() *myArray.length)];
 }
 
-function getPlayerChoice () {
+// Produces a Prompt for the player to select.
+
+function getPlayerChoice() {
     let input = prompt("Type Rock, Paper or Scissors");
     while (input == null) {
         input = prompt("Type Rock, Paper or Scissors");
     }
     input = input.toLowerCase();
-    console.log(input);
+    return input;
+}
+
+// Plays a Round.
+
+function playRound() {
+    const playerSelection = getPlayerChoice();
+    const computureSelection = getComputerChoice();
+    if (playerSelection === computureSelection) {
+        return "tie";
+    }
+    else if (playerSelection === "rock" && computureSelection === "paper"
+    || playerSelection === "paper" && computureSelection === "scissors" 
+    || playerSelection === "scissors" && computureSelection === "rock") {
+        return "Player loses!";
+    }
+    else {
+        return "Player Wins!";
+    }
 }
